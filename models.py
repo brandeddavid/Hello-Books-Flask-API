ca5574df400eef34947eb5d0b3188c08092fddeb
David Mwangi Mathenge <david.mathenge98@gmail.com>
2018-03-25 12:35:55 +0300

    Add Book and User models blueprint

--- models.py
+++ models.py
@@ -1,28 +1,36 @@
 from flask_restful import Resource
 import request
-import json
+import
 
-# class Users(object):
-#
-#     def __init__(self, fname, sname, username, password):
-#         self.firstname = fname
-#         self.sname = sname
-#         self.username = username
-#         self.password = password
-#
-#         self.users = []
-#
-#     class Createuser(Resource):
-#
-#         def get(self):
-#
-#             pass
-#
-#         def put(self):
-#
-#             pass
 books = []
-class Books(object):
+users = []
+
+class User(object):
+
+    def __init__(self):
+
+        self.users = {}
+
+    def createuser(self, fname, sname, username, password):
+        pass
+        # self.firstname = fname
+        # self.sname = sname
+        # self.username = username
+        # self.password = password
+
+    def loginuser(self, username, password):
+        pass
+
+    def updatepassword(self, username, oldpassword, newpassword):
+        pass
+
+    def deleteuser(self, username, password):
+        pass
+
+    def borrowbook(self, username, password, bookid):
+        pass
+
+class Book(object):
     """"
     """
 

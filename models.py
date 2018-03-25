d323be793254ed711e3fb5ad6e531452edf0fadf
David Mwangi Mathenge <david.mathenge98@gmail.com>
2018-03-25 18:56:36 +0300

    Add User and Book Models logic

--- models.py
+++ models.py
@@ -1,7 +1,3 @@
-from flask_restful import Resource
-import request
-import
-
 books = []
 users = []
 
@@ -11,23 +7,78 @@ class User(object):
 
         self.users = {}
 
-    def createuser(self, fname, sname, username, password):
-        pass
-        # self.firstname = fname
-        # self.sname = sname
-        # self.username = username
-        # self.password = password
+    def createUser(self, fname, sname, username, password):
 
-    def loginuser(self, username, password):
-        pass
+        user = {}
+        user['firstName'] = fname
+        user['secondName'] = sname
+        user['password'] = password
 
-    def updatepassword(self, username, oldpassword, newpassword):
-        pass
+        try:
 
-    def deleteuser(self, username, password):
-        pass
+            self.users[username] = user
+            users.append(self.users)
 
-    def borrowbook(self, username, password, bookid):
+        except Exception as e:
+
+            return 'User Already Exists'
+
+        finally:
+
+            return 'User Created Successfully'
+
+    def loginUser(self, username, password):
+
+        for user in users:
+
+            if username in user.keys():
+
+                if user[username]['password'] == password:
+
+                    return 'User Login Successful'
+
+                else:
+
+                    return 'Passwords do not Match'
+
+            else:
+
+                return 'User Not Registered'
+
+    def updatePassword(self, username, oldpassword, newpassword):
+
+        for user in users:
+
+            if username in user.keys():
+
+                if user[username]['password'] == oldpassword:
+
+                    user[username]['password'] = newpassword
+
+                    return 'Password Reset Successful'
+
+                else:
+
+                    return 'Password Mismatch'
+
+            else:
+
+                return 'User Not Registered'
+
+    def deleteUser(self, username):
+
+        for user in users:
+
+            if username in user.keys():
+
+                del user[username]
+
+                return 'User Deleted Successfully'
+            else:
+
+                return 'User Not Registered'
+
+    def borrowBook(self, username, password, bookid):
         pass
 
 class Book(object):
@@ -39,29 +90,54 @@ class Book(object):
         self.book = {}
 
     def createbook(self, id, title, author):
-        pass
+        bookDetails = {}
 
-        # bookDetails = {}
-        #
-        # bookDetails['Title'] = title
-        # bookDetails['Auhor'] = author
-        #
-        # self.book[id] = bookDetails
-        # books.append(self.book)
-        #
-        # return books
+        bookDetails['title'] = title
+        bookDetails['author'] = author
+
+        self.book[id] = bookDetails
+        books.append(self.book)
+
+        return 'Book Created Successfully'
 
     def deletebook(self, id):
-        pass
 
-    def updatebook(self,id):
-        pass
+        for book in books:
+
+            if id in book.keys():
+
+                del book[id]
+                return 'Book Deleted Successfully'
+
+            else:
+
+                return 'Book Not Available'
+
+    def updatebook(self,id, newTitle, newAuthor):
+
+        for book in books:
+
+            if id in book.keys():
+
+                book[id]['title'] = newTitle
+                book[id]['author'] = newAuthor
+
+                return 'Book Update Successful'
 
     def getbook(self, id):
-        pass
 
-b = Books()
+        for book in books:
 
-b.createbook(2, 'The Lean Start Up', 'Eric Ries')
+            if id in book.keys():
 
-print(books)
+                return 'Book Exists'
+
+            else:
+
+                return 'Book Does not Exist'
+
+# b = Books()
+#
+# b.createbook(2, 'The Lean Start Up', 'Eric Ries')
+#
+# print(books)

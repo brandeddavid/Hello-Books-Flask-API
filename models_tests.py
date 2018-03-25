493208a22606da3ac9af4bbc1f09c03865fc56f2
David Mwangi Mathenge <david.mathenge98@gmail.com>
2018-03-25 12:36:21 +0300

    Add User model unittest

--- models_tests.py
+++ models_tests.py
@@ -5,15 +5,20 @@ from models import *
 class TestBooksModel(unittest.TestCase):
 
     def setUp(self):
-        self.book = Books()
+        self.book = Book()
+        res = self.book.createbook('1', 'Game of Thrones', 'George R.R. Martin')
 
     def tearDown(self):
         pass
 
     def test_book_creation_successful(self):
 
-        res = self.book.createbook('1', 'Game of Thrones', 'George R.R. Martin')
+        res = self.book.createbook('2', 'Game of Thrones', 'George R.R. Martin')
         self.assertEqual(res, 'Book Created Successfully')
+
+    def test_book_already_exists(self):
+        res = res = self.book.createbook('1', 'Game of Thrones', 'George R.R. Martin')
+        self.assertEqual(res, 'Book Already Exists')
 
     def test_book_exists(self):
         res = self.book.getbook('1')
@@ -35,7 +40,37 @@ class TestBooksModel(unittest.TestCase):
 class TestUserModel(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.user = User()
+        self.user.createuser('David', 'Mwangi', 'dmwangi', 'password')
 
     def tearDown(self):
         pass
+
+    def test_user_creation_successful(self):
+        res = self.user.createuser('David', 'Mwangi', 'geekdave', 'password')
+        self.assertEqual(res, 'User Created Successfully')
+
+    def test_user_already_exists(self):
+        res = self.user.createuser('David', 'Mwangi', 'dmwangi', 'password')
+        self.assertEqual(res, 'User Already Exists')
+
+    def test_user_login(self):
+        res = self.user.loginuser('dmwangi', 'password')
+        self.assertEqual(res, 'User Login Successful')
+
+    def test_user_update_password(self):
+        res = self.user.updatepassword('dmwangi', 'password', 'newpassword')
+        self.assertEqual(res, 'Password Reset Successful')
+
+    def test_delete_user(self):
+        res = self.user.deleteuser('dmwangi', 'password')
+        self.assertEqual(res, 'User Deleted Successfully')
+
+    def test_book_borrowing(self):
+        res = self.user.borrowbook('dmwangi', 'password', '1')
+        self.assertEqual(res, 'Book Borrowed Successfully')
+
+
+if __name__ == '__main__':
+
+    unittest.main()

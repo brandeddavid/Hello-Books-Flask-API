6a071c34eb271537b50be4684b74db51edf989f0
David Mwangi Mathenge <david.mathenge98@gmail.com>
2018-03-27 18:26:47 +0300

    Update .travis.yml

--- run.py
+++ run.py
@@ -3,14 +3,20 @@ from api import routes
 
 app = Flask(__name__)
 
-app.config['SECRET-KEY'] = 'super-secret-key'
 app.register_blueprint(routes.mod)
 
 
 @app.route('/')
 def index():
+    """
+    Route to Index Page
+    :return: Hello World on Index Page
+    """
     return 'Hello World'
 
 
 if __name__ == '__main__':
+    """
+        Starts Flask Server Once File is run
+    """
     app.run(debug=True)

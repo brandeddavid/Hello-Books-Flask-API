16eea5ee7b89fd2ff27f81f4c1e214bcd2db63f2
David Mwangi Mathenge <david.mathenge98@gmail.com>
2018-04-02 00:36:23 +0300

    [Bug 156423423]Add Update Password Tests

--- tests.py
+++ tests.py
@@ -385,7 +385,6 @@ class TestUserAPI(unittest.TestCase):
         }
         res1 = self.app.post('/api/v1/auth/register', data=json.dumps(payload))
         res = self.app.post('/api/v1/auth/reset-password/1>', data=json.dumps({"username":"testuser1", "password":"newpassword"}))
-        print(res.data)
         self.assertEqual(res.status_code, 200)
 
     def test_update_password_user_not_exist(self):
@@ -393,12 +392,6 @@ class TestUserAPI(unittest.TestCase):
 
         :return:
         """
-        payload = {
-            "username": "testuser",
-            "password": "testpassword",
-            "admin": "True"
-        }
-        res1 = self.app.post('/api/v1/auth/register', data=json.dumps(payload))
         res = self.app.post('/api/v1/auth/reset-password/1>', data=json.dumps({"username":"usewwr", "password":"newpassword"}))
         self.assertEqual(res.status_code, 404)
 

b3d71481747f2082dee5e8dcca18637571ffbbcb
brandeddavid <david.mathenge98@gmail.com>
2018-05-22 16:03:57 +0300

    Add user api endpoints

--- api/routes.py
+++ api/routes.py
@@ -5,6 +5,7 @@
 # """
 from flask import Blueprint
 from flask_restful import Api
+from api.users.views import CreateUser, GetAllUsers
 
 mod = Blueprint('api', __name__)
 api = Api(mod)
@@ -12,8 +13,8 @@ api = Api(mod)
 # api.add_resource(Books, '/api/v1/books')
 # api.add_resource(BookOps, '/api/v1/books/<string:book_id>')
 # api.add_resource(BorrowBook, '/api/v1/users/books/<string:book_id>')
-# api.add_resource(GetAllUsers, '/api/v1/users')
-# api.add_resource(CreateUser, '/api/v1/auth/register')
+api.add_resource(GetAllUsers, '/api/v1/users')
+api.add_resource(CreateUser, '/api/v1/auth/register')
 # api.add_resource(LoginUser, '/api/v1/auth/login')
 # api.add_resource(LogoutUser, '/api/v1/auth/logout')
 # api.add_resource(UpdatePassword, '/api/v1/auth/reset-password/<string:user_id>')

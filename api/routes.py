bda306f89813d61fcf56b148697fc5ccb0a12c68
brandeddavid <david.mathenge98@gmail.com>
2018-05-24 16:15:22 +0300

    Add book ops API endpoints

--- api/routes.py
+++ api/routes.py
@@ -6,13 +6,13 @@
 from flask import Blueprint
 from flask_restful import Api
 from api.users.views import CreateUser, GetAllUsers
-from api.books.views import Books
+from api.books.views import Books, BookOps
 
 mod = Blueprint('api', __name__)
 api = Api(mod)
 
 api.add_resource(Books, '/api/v1/books')
-# api.add_resource(BookOps, '/api/v1/books/<string:book_id>')
+api.add_resource(BookOps, '/api/v1/book/<book_id>')
 # api.add_resource(BorrowBook, '/api/v1/users/books/<string:book_id>')
 api.add_resource(GetAllUsers, '/api/v1/users')
 api.add_resource(CreateUser, '/api/v1/auth/register')

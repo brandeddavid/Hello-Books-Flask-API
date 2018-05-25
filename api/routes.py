47bc7f6c5977b174bc7a9bdeb44a28bd1090b77b
brandeddavid <david.mathenge98@gmail.com>
2018-05-25 17:49:13 +0300

    Add Login route

--- api/routes.py
+++ api/routes.py
@@ -5,8 +5,9 @@
 # """
 from flask import Blueprint
 from flask_restful import Api
-from api.users.views import CreateUser, GetAllUsers
+from api.users.views import GetAllUsers
 from api.books.views import Books, BookOps
+from api.auth.views import Register, Login
 
 mod = Blueprint('api', __name__)
 api = Api(mod)
@@ -15,7 +16,7 @@ api.add_resource(Books, '/api/v1/books')
 api.add_resource(BookOps, '/api/v1/book/<book_id>')
 # api.add_resource(BorrowBook, '/api/v1/users/books/<string:book_id>')
 api.add_resource(GetAllUsers, '/api/v1/users')
-api.add_resource(CreateUser, '/api/v1/auth/register')
-# api.add_resource(LoginUser, '/api/v1/auth/login')
+api.add_resource(Register, '/api/v1/auth/register')
+api.add_resource(Login, '/api/v1/auth/login')
 # api.add_resource(LogoutUser, '/api/v1/auth/logout')
 # api.add_resource(UpdatePassword, '/api/v1/auth/reset-password/<string:user_id>')

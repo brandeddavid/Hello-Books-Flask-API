0be57488e8328296eff288dfddfdc8a77ada065e
David Mwangi <dmwangi@Davids-MacBook-Pro.local>
2018-06-10 00:06:38 +0300

    Add non-admin Book resources

--- api/books/views.py
+++ api/books/views.py
@@ -3,7 +3,7 @@ from flask import json, request, Response
 from api.models import Book
 
 
-class Books(Resource):
+class GetBooks(Resource):
     """[summary]
     
     Arguments:
@@ -13,13 +13,13 @@ class Books(Resource):
         [type] -- [description]
     """    
     def get(self):
-        books = Book.all_books()
+        books = Book.get_all_books()
         if len(books) == 0:
             return Response(json.dumps({"Message": "No books found"}), status=404)
         return Response(json.dumps({"Books": [book.serialize for book in books]}), status=200)
 
 
-class BookOps(Resource):
+class GetBook(Resource):
     """[summary]
     
     Arguments:
@@ -29,15 +29,8 @@ class BookOps(Resource):
         try:
             book_id = int(book_id)
         except Exception as e:
-            return Response(json.dumps({"Message": "Invalid argument passed"}))
-        book = Book.book_by_id(id=book_id)
+            return Response(json.dumps({"Message": "Invalid argument passed"}), status=403)
+        book = Book.get_book_by_id(id=book_id)
         if not book:
             return Response(json.dumps({"Message": "Book does not exist"}), status=404)
         return Response(json.dumps(book.serialize), status=200)
-
-    def put(self, book_id):
-        pass
-    
-    def delete(self, book_id):
-        pass
-

373a6246b1f7d737d2a34fedc42fe1d76fe06040
David Mwangi <dmwangi@Davids-MacBook-Pro.local>
2018-06-12 22:22:12 +0300

    Add system seeders

---
+++ seeds/users.py
@@ -0,0 +1,7 @@
+
+
+user1 = User('josephk@yahoo.com', 'josephk', 'Joseph', 'Kenneth', 'Jkenneth@1997').save()
+user2 = User('alicewalker@gmail.com', 'alice','Alice', 'Walker', 'alicewalker12').save()
+user3 = User('peterparker@spyder.com', 'pparker', 'Peter', 'Parker', 'spiderman2').save()
+user4 = User('sheilam@yahoo.com', 'sheilam', 'Sheila', 'Mwatha', 'mwathasheila87').save()
+user5 = User('dank@gmail.com', 'dankeith','Dan', 'Keith', 'dankeith@0987').save()

da42831a748ccb328c81c381c10153bfeb4a221d
brandeddavid <david.mathenge98@gmail.com>
2018-06-17 11:27:50 +0300

    Add configuration file

---
+++ config.py
@@ -0,0 +1,71 @@
+import os
+
+
+class Config(object):
+    """[summary]
+    
+    Arguments:
+        object {[type]} -- [description]
+    """
+
+    DEBUG = False
+    CSRF_ENABLED = True
+    TESTING = False
+    JWT_SECRET_KEY = 'super-secret-key'
+    JWT_BLACKLIST_ENABLED = False
+    JWT_BLACKLIST_TOKEN_CHECKS = ['access']
+    SQLALCHEMY_DATABASE_URI = 'postgresql://dmwangi:postgres@localhost/hellobooks_db'
+
+
+class DevelopmentConfig(Config):
+    """[summary]
+    
+    Arguments:
+        Config {[type]} -- [description]
+    """
+
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://dmwangi:postgres@localhost/hellobooks_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestingConfig(Config):
+    """[summary]
+    
+    Arguments:
+        Config {[type]} -- [description]
+    """
+
+    TESTING = True
+    DEBUG = True
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/hellobooks_test'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://dmwangi:postgres@localhost/hellobooks_test'
+
+
+class StagingConfig(Config):
+    """[summary]
+    
+    Arguments:
+        Config {[type]} -- [description]
+    """
+
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    """[summary]
+    
+    Arguments:
+        Config {[type]} -- [description]
+    """
+
+    DEBUG = False
+    TESTING = False
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'staging': StagingConfig,
+    'production': ProductionConfig
+}

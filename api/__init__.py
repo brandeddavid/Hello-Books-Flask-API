8736d0e3862799717ee844d41d41658c5c88f33c
brandeddavid <david.mathenge98@gmail.com>
2018-06-17 11:43:15 +0300

    Refactor api __init__ file

--- api/__init__.py
+++ api/__init__.py
@@ -3,6 +3,8 @@
   init file
 ]
 """
+from config import app_config
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -11,11 +13,8 @@ from flask_jwt_extended import JWTManager
 app = Flask('__name__')
 
 jwt = JWTManager(app)
-app.config['JWT_SECRET_KEY'] = 'super-secret-key'
-app.config['JWT_BLACKLIST_ENABLED'] = False
-app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access']
+app.config.from_object(app_config['development'])
 app.url_map.strict_slashes = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dmwangi:postgres@localhost/hellobooks_db'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)

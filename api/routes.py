e8cee693a3d3bdd80a17ca7109726eacaf6d96fe
brandeddavid <david.mathenge98@gmail.com>
2018-06-17 21:51:04 +0300

    Add promote user api endpoint

--- api/routes.py
+++ api/routes.py
@@ -5,7 +5,7 @@
 # """
 from flask import Blueprint
 from flask_restful import Api
-from api.users.views import GetAllUsers, BorrowOps, BorrowHistory
+from api.users.views import GetAllUsers, BorrowOps, BorrowHistory, PromoteUser
 from api.admin.views import AddBook, BookOps
 from api.books.views import GetBooks, GetBook
 from api.auth.views import Register, Login, Logout, ResetPassword
@@ -24,3 +24,4 @@ api.add_resource(Register, '/api/v1/auth/register')
 api.add_resource(Login, '/api/v1/auth/login')
 api.add_resource(Logout, '/api/v1/auth/logout')
 api.add_resource(ResetPassword, '/api/v1/auth/reset-password')
+api.add_resource(PromoteUser, '/api/v1/user/promote')
